c44cda89a57bafba456db6dece7a7a0c7a755232
Lou-trinh <loutrinh2312000@gmail.com>
2026-06-17 12:13:10 +0700

    Fix process.env for Cloudflare build: read from actual env vars

--- webpack.common.js
+++ webpack.common.js
@@ -4,11 +4,16 @@ const path = require('path');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
 
-const env = dotenv.config().parsed || {};
-const envKeys = Object.keys(env).reduce((acc, key) => {
-  acc[`process.env.${key}`] = JSON.stringify(env[key]);
-  return acc;
-}, {});
+// Merge .env file (local) with actual process.env (Cloudflare build vars)
+dotenv.config();
+const env = {
+  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY || '',
+  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN || '',
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || '',
+  FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET || '',
+  FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
+  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID || '',
+};
 
 module.exports = {
   entry: {
@@ -20,6 +25,6 @@ module.exports = {
     filename: 'js/[name].js',
   },
   plugins: [
-    new webpack.DefinePlugin(envKeys),
+    new webpack.DefinePlugin({ 'process.env': JSON.stringify(env) }),
   ],
 };
